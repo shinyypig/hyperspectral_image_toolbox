0c936be8f45d477b72a4a030da54ced378820182
Liangliang Zhu <shinyypig@gmail.com>
2023-03-05 12:36:21 +0800

    ✨ 添加新的初始化代码

--- install.m
+++
@@ -1,9 +0,0 @@
-addpath('Band Selection');
-addpath('Data');
-addpath('Examples');
-addpath('Target Detection');
-addpath('Utils');
-addpath('Dimension Reduction');
-addpath('Endmember Extraction');
-addpath('Clustering');
-addpath('Data Analysis');
---
+++ startup.m
@@ -0,0 +1,9 @@
+addpath(genpath('./'));
+set(groot, 'defaultLineLineWidth', 2);
+set(groot, 'DefaultLineMarkerSize', 8);
+set(groot, 'defaultAxesFontSize', 18);
+set(groot, 'defaultAxesXGrid', 'on');
+set(groot, 'defaultAxesYGrid', 'on');
+set(groot, 'defaultAxesBox', 'on');
+set(groot, 'defaultLegendBox', 'off');
+format compact;
